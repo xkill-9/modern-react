cfbe50c94754256fc6632d198caaa17fbf9c80ae
Jorge Ivan Morales <jorge.iv.morales@gmail.com>
2016-07-28 14:03:02 -0500

    Added youtube-api-search dependencie and update to the package.json git field

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './client/App';
 
+const API_KEY = 'AIzaSyBDOQACMO6HVoAWcOX7iAwJomVb79kmhyI';
+
 ReactDOM.render(<App />, document.getElementById('react-container'));
